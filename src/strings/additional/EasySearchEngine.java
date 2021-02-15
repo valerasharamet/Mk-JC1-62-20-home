838603124bb022428678702f9cde6f054ebb4aff
Valerry Sharamet <valerasharamet@gmail.com>
2021-02-15 20:40:56 +0300

    Done

--- src/strings/additional/EasySearchEngine.java
+++ src/strings/additional/EasySearchEngine.java
@@ -2,20 +2,31 @@ package strings.additional;
 
 public class EasySearchEngine implements ISearchEngine {
     private int count;
-    private int index = 0;
-    private int index2 =0;
+    private int index = 1;
+    private int index2 = 0;
+    private boolean rip = false;
 
     @Override
     public long search(String text, String word) {
 
         while (index != -1) {
-            index = text.indexOf(" " + word + " ", index2);
-            if(index >= 0) {
+            index = text.indexOf(word, index2);
+            if (index >= 0 && index <= text.length()) {
+                int b = text.codePointAt(index - 1);
+                int a = text.codePointAt(index + 1);
+                if (false == b >= 1040 && b < 1103) {
+                    if (false == a >= 1040 && a < 1103) {
+
+                        count++;
+                    }
+                }
+                //if (false ==((b >= 1040 && b < 1103 && b == 1025) && (a >= 1040 && a < 1103 && a == 1025))) {
+
+                //     count++;
+                //}
                 index2 = index;
                 index2++;
-                count++;
             }
-
 
         }
         System.out.println(count);
